2819fa2a2698194e198fcd0b44c79d480ec8e2e1
RePozitive <43346345+RePozitive@users.noreply.github.com>
2021-03-24 20:39:20 +0300

    Update Practic1.py

--- Practic1.py
+++ Practic1.py
@@ -1,9 +1,9 @@
-def FunctionSum(list, value=0):
-    if len(list) == value:
-        return 0
-    else:
-        return sum(list)
+def FunctionSum(list, sum=0):
+    for i in list:
+        sum += i
+    return sum
 
 array = [1, 2, 3, 4, 5]     # 15
 array2 = [5, -3, 4, -10]    # -4
+array3 = []                 # 0
 print(FunctionSum(array2))
